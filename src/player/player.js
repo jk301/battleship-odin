8e97e6dd404871a3d84f90e9d515a782b1beeb7a
junaid <jk7948677@gmail.com>
2026-05-22 17:38:50 +0530

    Reverted back to just adjacent shots

--- src/player/player.js
+++ src/player/player.js
@@ -4,6 +4,7 @@ export function Player () {
 
     let board = gameBoard()
     let attemShots = []
+    let huntIndex = 0
 
     function attack (opponent, coords) {
         attemShots.push(coords)
@@ -24,29 +25,36 @@ export function Player () {
         return opponent.board.receiveAttack([num1, num2])
     }
 
+
     function adjAttack (opponent) {
-        const hits = opponent.board.getHitShots()
+        const hits = opponent.board.getHitShots().slice(huntIndex)
 
         if (hits.length === 0) {
             return randomAttack(opponent)
-        } else {
-            const lastHit = hits.at(-1)
-            let validShots = [
-                [lastHit[0] + 1, lastHit[1]],
-                [lastHit[0] - 1, lastHit[1]],
-                [lastHit[0], lastHit[1] + 1],
-                [lastHit[0], lastHit[1] - 1],
-            ]
-
-            validShots = validShots.filter(i => i[0] >= 0 && i[0] <= 9 && i[1] >= 0 && i[1] <= 9)
-            validShots = validShots.filter(i => !attemShots.some(k => k[0] === i[0] && k[1] === i[1]))
-
-            if (validShots.length === 0) {
-                return randomAttack(opponent)
-            } else {
-                return attack(opponent, validShots[0])
-            }
         }
+
+        const lastHit = hits.at(-1)
+
+        let validShots = [
+            [lastHit[0] + 1, lastHit[1]],
+            [lastHit[0] - 1, lastHit[1]],
+            [lastHit[0], lastHit[1] + 1],
+            [lastHit[0], lastHit[1] - 1],
+        ]
+
+        validShots = validShots.filter(i => i[0] >= 0 && i[0] <= 9 && i[1] >= 0 && i[1] <= 9)
+        validShots = validShots.filter(i => !attemShots.some(k => k[0] === i[0] && k[1] === i[1]))
+
+        if (validShots.length === 0) {
+            huntIndex = opponent.board.getHitShots().length
+            return randomAttack(opponent)
+        }
+
+        const result = attack(opponent, validShots[0])
+        if (result === 'hit' && opponent.board.isShipSunkAt(validShots[0])) {
+            huntIndex = opponent.board.getHitShots().length
+        }
+        return result
     }
 
 
