21bc486bbda4e3c76b40d25c7734b2a21cbe9c8c
junaid <jk7948677@gmail.com>
2026-05-22 14:30:16 +0530

    Added game over screen (which lets you restart)

---
+++ src/pages/game_over/game_over.js
@@ -0,0 +1,22 @@
+// game_over.js
+import "./game_over.css"
+
+export function gameOver (winner, choice,  onRestart) {
+    const main = document.createElement('div')
+    main.classList.add('game-over')
+
+    const winScreen = document.createElement('div')
+    winScreen.textContent = `${winner} WON !! `
+
+    const tryAgain = document.createElement('button')
+    tryAgain.textContent = `Restart`
+
+    tryAgain.addEventListener('click', () => {
+        onRestart()
+    })
+
+    main.appendChild(winScreen)
+    main.appendChild(tryAgain)
+
+    return main
+}
