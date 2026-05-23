4eb9fcf218bb3188abee1b7026df8eb070c5d969
junaid <jk7948677@gmail.com>
2026-05-23 12:15:19 +0530

    Added Player turns

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { select_screen } from "./pages/select_screen/select_screen.js";
 import { placements } from "./pages/placements/placements.js";
 import { Player } from './player/player.js'
 import { gameScreen } from "./pages/game_screen/game_screen.js";
+import { p1_gameScreen } from "./pages/game_screen/p1_game_screen.js";
+import { p2_gameScreen } from "./pages/game_screen/p2_game_screen.js";
 import { randomPlacement } from "./pages/random/random.js";
 import { passScreen } from "./pages/pass_screen/pass_screen.js";
 
@@ -12,6 +14,18 @@ const root = document.getElementById('root')
 function showNow (page) {
     root.innerHTML = ""
     root.appendChild(page)
+}
+
+function playerTurns(P1, P2, choice) {
+    showNow(p1_gameScreen(P1, P2, choice, () => startGame(choice), () => {
+        showNow(passScreen('Player - 2', () => {
+            showNow(p2_gameScreen(P1, P2, choice, () => startGame(choice), () => {
+                showNow(passScreen('Player - 1', () => {
+                    playerTurns(P1, P2, choice)
+                }))
+            }))
+        }))
+    }))
 }
 
 function startGame(choice) {
@@ -24,7 +38,7 @@ function startGame(choice) {
                 showNow(passScreen('Player - 2', () => {
                     showNow(placements(choice, P2, 'Player 2', () => {
                         showNow(passScreen('Player - 1', () => {
-                            showNow(gameScreen(P1, P2, choice, () => startGame(choice)))
+                            playerTurns(P1, P2, choice)
                         }))
                     }))
                 }))
